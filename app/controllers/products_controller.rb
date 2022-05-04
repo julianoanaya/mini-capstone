59284afa0cefd481a0367fc5267fb24e5ff30347
Julian Anaya <juliananaya531@gmail.com>
2022-05-04 10:18:07 -0700

    add routes that show only one of the products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,13 @@ class ProductsController < ApplicationController
     def all_products_method
         render(json: Product.all)
     end
+    def tv_method
+        render(json: Product.first)
+    end
+    def Ps5_method
+        render(json: Product.)
+    end
+    def Mac_Book_method
+        render(json: Product.last)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/all_products", controller:"products", action: "all_products_method"
+  get "/tv", controller:"products", action: "tv_method"
+  get "/Ps5", controller:"products", action: "Ps5_method"
+  get "/Mac_Book", controller:"products", action: "Mac_Book_method"
+
 end
